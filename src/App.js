9b94b36b6c737017089842b79bcf65a28a6fb4eb
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-08-04 15:44:33 -0400

    calculator project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-
-import {useState} from 'react'
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  //let count = 0;
-  const [count, setCount] = useState(0)
+  const [total, setTotal] = useState(0);
+
+  let digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>My First React App</h1>
-        <p>
-          You clicked the button {count} times.
-        </p>
-        <button
-          onClick={() => {
-            console.log("I clicked the button" + count);
-            //count = count + 1;
-            setCount(count +1)
-          }}
-        >Add one</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         
-        </a>
+        <div>Total: {total} </div>
+          {digits.map((digit) => {
+            return <button key={digit} onClick={()=> {
+              setTotal(total + digit)}
+            }>{digit}</button>
+          })}
+
+          <button onClick={() => {setTotal(total + 1);}}> reset</button>
+        {/*
+        <button onClick={() => {setTotal(total + 1);}}>1</button>
+        <button onClick={() => {setTotal(total + 2);}}>2</button>
+        <button onClick={() => {setTotal(total + 3);}}>3</button>
+        <button onClick={() => {setTotal(total + 4);}}>4</button>
+        <button onClick={() => {setTotal(total + 5);}}>5</button>
+        <button onClick={() => {setTotal(total + 6);}}>6</button>
+        <button onClick={() => {setTotal(total + 7);}}>7</button>
+        <button onClick={() => {setTotal(total + 8);}}>8</button>
+        <button onClick={() => {setTotal(total + 9);}}>9</button>
+        <button onClick={() => {setTotal(total + 0);}}>0</button>
+        <button onClick={() => {setTotal(total + 0);}}>
+        <button onClick={() => {setTotal(total + 0);}} */}
+
       </header>
     </div>
   );
